86d5b742b0084bf0f77b2c6cbb48f4c3852340bf
BelvieCabrel <bdoukeutchouami@gmail.com>
2024-05-21 00:49:30 +0200

    challenge39

--- test.c
+++ test.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
 void main(){
-    printf("welcome");
+    int a,b;
+    printf("entrer deux nomnres");
+    scanf("%d,%d",&a,&b);
+    printf("dividenter = %d, divior=%d", a,b);
+
+
+    if (a%b==0){
+        printf("output: true");
+    }
+    else{
+        printf("output: false");
+    }
 }
